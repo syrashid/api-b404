19e09ae21da21c102ff70b496db88ad32c76cc8f
Sy Rashid <sy.rashid.11@gmail.com>
2020-06-09 09:49:53 +1000

    FEAT: added index controller action and view

---
+++ app/controllers/api/v1/restaurants_controller.rb
@@ -0,0 +1,6 @@
+# app/controllers/api/v1/restaurants_controller.rb
+class Api::V1::RestaurantsController < Api::V1::BaseController
+  def index
+    @restaurants = policy_scope(Restaurant)
+  end
+end
