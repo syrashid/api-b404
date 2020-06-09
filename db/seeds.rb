cf9d5bb57223340c01709dbcdac7421d8c631e97
Sy Rashid <sy.rashid.11@gmail.com>
2020-06-09 10:02:57 +1000

    FEAT: Completed index api action

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(email: 'sy@gmail.com', password: 'password')
+User.create(email: 'nico@gmail.com', password: 'password')
+User.create(email: 'sheila@gmail.com', password: 'password')
+
+Restaurant.create(name: "I Love Dumplings", address: '298 Bridge Rd, Richmond VIC 3121, Australia', user: User.first)
+Restaurant.create(name: "Pick A Pokit", address: '106 Swan St, Cremorne VIC 3121, Australia', user: User.first)
+Restaurant.create(name: "Feast of Merit", address: '2117 Swan St, Richmond VIC 3121, Australia', user: User.first)
